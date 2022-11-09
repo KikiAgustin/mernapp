ba22e4b1b95033d7cafbb17c86679572b765a3ba
kikiAgustin <kikiagustin62@gmail.com>
2022-11-09 07:33:17 +0600

    clean up refactor component detail

---
+++ components/organisms/SideBar/MenuItem.tsx
@@ -0,0 +1,25 @@
+import Image from "next/image";
+
+interface MenuItemProps {
+  title: string;
+}
+export default function MenuItem(props: MenuItemProps) {
+  const { title } = props;
+  return (
+    <div className="item active mb-30">
+      <div className="me-3">
+        <Image
+          src="/icon/icon-menu-overview.svg"
+          width={25}
+          height={25}
+          alt="icon menu"
+        />
+      </div>
+      <p className="item-title m-0">
+        <a href="" className="text-lg text-decoration-none">
+          {title}
+        </a>
+      </p>
+    </div>
+  );
+}
